a6205fcd7dd55230b7e755ecaedc8624bd3d69e5
Zero <jrr043@latech.edu>
2019-01-15 09:54:47 -0600

    Added ternary to script so that if a user's name is over 20 char, it won't apply it to the header due to scroll bar

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ const imghead = document.getElementById('bgtxth1');
 const imgparagraph = document.getElementById('bgtxtp');
 document.getElementById('submitbtn').addEventListener('click', function(e){
     e.preventDefault();
-    imghead.innerHTML = `Thanks, ${document.getElementById('box1').value}`;
+    imghead.innerHTML = (document.getElementById('box1').value.length < 20 ? `Thanks, ${document.getElementById('box1').value}` : `Thanks for signing up!`);
     imghead.style.display = "flex";
     imghead.style.justifyContent = "center";
     imgparagraph.innerHTML = `You will receive updates at your email: ${document.getElementById('box2').value}.`;
     imgparagraph.style.display = "block";
     document.getElementById('username').innerHTML = `Hi, ${document.getElementById('box1').value}`;
+    console.log;
     // return false;
 });
