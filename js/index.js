db0747f5292f4c611bd897fb36669c78603815bf
samuel <osamaimafidon@gmail.com>
2019-08-16 11:29:31 +0100

    add form modal to page

--- js/index.js
+++ js/index.js
@@ -5,6 +5,19 @@ function init() {
     window.addEventListener('scroll',function(e) {
     nav.classList.add("lighten-nav");
     if(window.scrollY === 0) nav.classList.remove("lighten-nav");
+});
+
+    const modal = document.querySelector(".modal");
+    setTimeout(function(){
+        modal.style.display = 'block';
+        modal.style.transition = '5s ease-in';
+        // modal.classList.add("modal-ease-in");
+    },2000)
+
+    const close = document.querySelector(".close");
+    close.addEventListener('click',function(){
+        modal.style.display = 'none';
+        modal.style.transition = '1s ease-out';
     })
 }
 
